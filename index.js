bd4a6fa40c1f1df895e1c1b193114bef3102a420
Marissa <35174396+mUtterberg@users.noreply.github.com>
2018-04-02 01:03:02 -0400

    Add initial dev files via upload
    New Files:
    index.js
    careers.json - Working data file with selections from full Occupational Outlook Handbook JSON file
    ooh.json - Raw data converted from XML
    xml-compilation.xml - Full XML data for 2016 OOH downloaded from BLS website.
    
    Sources:
    https://www.bls.gov/ooh/
    https://www.bls.gov/ooh/about/ooh-developer-info.htm
    https://www.bls.gov/ooh/xml-compilation.xml <--XML of Occupational Outlook Handbook

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+* HTTP Cloud Function.
+*
+* @param {Object} req Cloud Function request context.
+* @param {Object} res Cloud Function response context.
+*/
+
+// Include JSON data
+jQuery.getJSON('careers.json').done(function (data) {
+  careers = data;
+  careerToday = careers[Math.floor(Math.random() * careers.length)];
+})
+
+/*// Pick random career title
+function pickRandomCareer(){
+        window.selectedCareer = window.careers[Math.floor(Math.random() * window.careers.length)];
+        console.log(window.selectedCareer);
+        console.log(window.careers);
+    }
+
+// Define a career of the day
+var careerToday = pickRandomCareer()*/
+
+exports.helloHttp = function helloHttp (req, res) {
+  response = "Today's career spotlight is " + careerToday.Title //Default response from the webhook to show it's working
+
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+};
